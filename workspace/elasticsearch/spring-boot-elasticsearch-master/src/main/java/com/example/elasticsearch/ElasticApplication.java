eabafd2bc0d7fcf681be6f8e6456ff16efbb822e
Ganesh <ganeshshetty21@gmail.com>
2019-07-22 21:31:43 -0500

    initial commit

---
+++ workspace/elasticsearch/spring-boot-elasticsearch-master/src/main/java/com/example/elasticsearch/ElasticApplication.java
@@ -0,0 +1,14 @@
+package com.example.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://dzone.com/articles/elasticsearch-with-spring-boot-application
+
+@SpringBootApplication
+public class ElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticApplication.class, args);
+	}
+}
